43a32ec633d5947ffa8b9d78fa8f6cc2c3180916
Todd Hainsworth <hainsworth.todd@gmail.com>
2013-12-06 00:00:14 +1030

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name "Todd Hainsworth"
-    email "todd@example.com"
+    sequence(:name) { |n| "Person #{1}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
